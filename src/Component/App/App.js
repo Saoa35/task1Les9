889643a6c829eafacfaec157b35acc241a167eea
Saoa35 <saoamah@gmail.com>
2022-05-27 10:42:59 +0200

    add Error @ Example error components

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,9 +1,15 @@
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import ExampleError from '../ExampleError/ExampleError';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+      <ErrorBoundary>
+        <ExampleError/>
+      </ErrorBoundary>
+
     </div>
   );
 }
