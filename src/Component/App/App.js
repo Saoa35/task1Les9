6425e358c7d17ae0886f4c7815096bbd8734c5a9
Saoa35 <saoamah@gmail.com>
2022-05-27 11:12:01 +0200

    create clock component

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,3 +1,4 @@
+import Clock from '../Clock/Clock';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import ExampleError from '../ExampleError/ExampleError';
 import './App.css';
@@ -5,6 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <Clock/>
       
       <ErrorBoundary>
         <ExampleError/>
