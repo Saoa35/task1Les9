16af21305c0c019a1bfa3ea971c1ab76e8fa259b
Saoa35 <saoamah@gmail.com>
2022-05-27 10:38:43 +0200

    create ExampleError component

---
+++ src/Component/ExampleError/ExampleError.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+
+export default class ExampleError extends Component {
+
+    constructor(props) {
+        super(props);
+        state = {
+            isError: false
+        }
+        this.handleCreateError = this.handleCreateError.bind(this);
+    }
+
+    handleCreateError() {
+        this.setState({
+            isError: true
+        })
+    }
+
+    render() {
+
+        if(this.state.isError) {
+            throw new Error('Error in Example Error!');
+        }
+
+        return(
+            <div>
+                <h2>Example Error Component</h2>
+                <p>
+                    <button onClick={this.handleCreateError}>
+                        Create Error
+                    </button>
+                </p>
+            </div>
+        )
+    }
+}
